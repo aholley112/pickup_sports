c835b78211ca6abf4ad11730cc7cd1a3601e0e48
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-27 21:42:50 -0600

    coding with video

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true
+    validates :username, presence: true, uniqueness: true
     validates: :email, presence: true
     validates: :first_name, presence: true
     validates: :last_name, presence: true
