edfaa84340892c96261ea0010658947c8fa630b6
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-28 18:34:16 -0600

    coding with video

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
+    validate: validate_username 
     validates: :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 255 }, format: {
         with: URI::MailTo::EMAIL_REGEXP
     }
