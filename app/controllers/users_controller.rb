668389277a03796eb56dfa87c26fb70cd1698165
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-30 21:26:14 -0600

    coding with video

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
 
-    
+    render json: user, status: 200
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :first_name, :last_name, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ get '/users', to: 'users#index'
 
 # localhost:3000/users/1
 get '/users/:id', to: 'users#show'
+
+# localhost:3000/users
+post '/users', to: 'users#create'
 end
