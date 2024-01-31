c8afed8c300e1ffeab0befef483fd07fd0f1c1b8
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-30 21:20:35 -0600

    coding with video

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
 
     render json: users, status: 200
   end
+
+  def show
+    user = User.find(params[:id])
+
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
 # localhost:3000/users
 get '/users', to: 'users#index'
+
+# localhost:3000/users/1
+get '/users/:id', to: 'users#show'
 end
