b31cd58ac67887b018f92b8a03b3e0380be1ed8e
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-06 15:45:42 -0600

    Coding with Video

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   validates :start_date_time, :end_date_time, :guests, presence: true
+  validates :title, presence: true
+
+  validate :start_date_time_cannot_be_in_the_past, end_date_time_cannot_be_before_start_date_time
 
   # associations
   belongs_to :user
@@ -11,4 +14,15 @@ class Event < ApplicationRecord
 
   has_and_belongs_to_many :sports
 
+  def start_date_time_cannot_be_in_the_past
+    if start_date_time.present? && start_date_time < DateTime.now
+      errors.add(:start_date_time, "can't be in the past")
+    end
+  end
+
+  def end_date_time_cannot_be_before_start_date_time
+    if end_date_time.present? && end_date_time < start_date_time
+      errors.add(:end_date_time, "can't be before start date time")
+    end
+  end
 end
