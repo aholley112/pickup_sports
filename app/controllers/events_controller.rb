3926f0c524420f9222d9346c64cb464d06c2b90d
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-06 16:16:30 -0600

    Coding with Video

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,49 @@
 class EventsController < ApplicationController
+
+    before_action :set_event, only: [:show, :update, :destroy]
+
     def index
+    events = Event.all
+    render json: events, status: :ok
+    end
+
+    def show
+        render json: @event, status: :ok
+    end
+end
+
+def create
+    event = Event.new(event_params)
+
+    if events.save
+        render json: event, status: :created
+    else
+        render json: event.errors, status: :unprocessable_entity
+    end
+end
+
+def update
+    if @event.update(event_params)
+        render json: @event, status: :ok
+    else
+        render json: @event.errors, status: :unprocessable_entity
+    end
+end
+
+def destroy
+ if @event.destroy
+    render json: nil, status: :ok
+ else
+    render json: @event.errors, status: :unprocessable_entity
+ end
+end 
+
+private
+
+def set_event
+    @event = Event.find(params[:id])
+end
+
+def event_params
+    params.permit(:title, :start_date_time, :end_date_time, :guests, :user_id, :sport_ids => [])
 end
